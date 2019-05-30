db18a224d3102d3c73fd6e1774b91a587d41e1b1
Ahn Jaechan <jaechan5256@gmail.com>
2019-05-30 16:46:08 +0900

    feature: global variables separated

---
+++ gvar.c
@@ -0,0 +1,9 @@
+//
+// Created by 안재찬 on 2019-05-30.
+//
+
+#include "param.h"
+#include "queue.h"
+#include "proc.h"
+#include "spinlock.h"
+#include "gvar.h"
---
+++ gvar.h
@@ -0,0 +1,18 @@
+//
+// Created by 안재찬 on 2019-05-29.
+//
+
+#ifndef XV6_PUBLIC_GVAR_H
+#define XV6_PUBLIC_GVAR_H
+
+typedef struct ptable_t {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable_t;
+
+ptable_t ptable;
+mlfq_t pmlfq;
+heap_t pheap;
+struct proc *mlfqproc;
+
+#endif //XV6_PUBLIC_GVAR_H
